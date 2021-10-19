6f8a8229ee342485dcbb277ad75228c3ecf40d7f
alfu32 <ioan.alferaru@gmail.com>
2021-10-19 20:23:03 +0200

    1 file modified

--- src/Board.js
+++ src/Board.js
@@ -11,7 +11,7 @@ export default function Board({ alphabet, size, children }) {
         return (
           <div className={'row'}>
             {array.map((col, ci) => {
-              return <Cell val={(ri * size + ci).toString(size)} />;
+              return <Cell val={(ri * size + ci)/*.toString(size)*/} />;
               /*return <Cell val={(ri ? alphabet[ri] : '') + alphabet[ci]} />;*/
             })}
           </div>
